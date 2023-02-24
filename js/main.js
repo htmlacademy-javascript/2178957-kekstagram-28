a32a426b93838066e6c7ef453302e82f9287bb00
Вячеслав <vyacheslav.belov.01@mail.ru>
2023-02-24 16:46:31 +0300

    Исправил дз

--- js/main.js
+++ js/main.js
@@ -1,33 +1,15 @@
-function polinFunction () {
 
-	let word = 'ропорокорс';
+const numberStringFunction = function(perse,string) {
+  const parse = parseInt(string, 10);
+  return parse;
+};
+numberStringFunction(2023,'2022 год');
+// console.log(fun);
 
-	console.log (word.slice(0,1)); // Т
-	console.log (word.slice(1,2)); // о
-	console.log (word.slice(2,3)); // п
-	console.log (word.slice(3,4)); // о
-	console.log (word.slice(4,5)); // р
+// имяФункции('2023 год');            // 2023
+// имяФункции('ECMAScript 2022');     // 2022
+// имяФункции('1 кефир, 0.5 батона'); // 105
+// имяФункции('агент 007');           // 7
+// имяФункции('а я томат');           // NaN
 
-
-	console.log(word.slice(4,5)); // р
-	console.log(word.slice(3,4)); // о
-	console.log(word.slice(2,3)); // п
-	console.log(word.slice(1,2)); // о
-	console.log(word.slice(0,1)); // Т
-
-	if(word[0] !== word[4]){
-		return 'Это не палиндром'
-	}
-	else if (word[1] !== word[3]){
-		return 'Это не палиндром'
-	}
-	else{
-		return 'Это полиндром';
-	}
-
-}
-
-
-let isPolindrom = polinFunction();
-console.log(isPolindrom)
-
+let mass = [1,2,45,1,];
