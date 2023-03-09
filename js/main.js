aec91bd7f86cae31be37ab5ea0243308057ad66c
Вячеслав <vyacheslav.belov.01@mail.ru>
2023-03-09 13:59:48 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,15 +1,53 @@
+const COMMENT_LISTS = [ 'Всё отлично!','В целом всё неплохо', 'Но не всё',
+  'Когда вы делаете фотографию', 'хорошо бы убирать палец из кадра', 'В конце концов это просто непрофессионально',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!)',];
+const DESCRIPTIONS = ['отдых', 'Море', 'чил','travel','Красота',];
+const NAMES = ['Сергей', 'Людмила', 'Ирина', 'Светлана',];
+const NUMBERS_PHOTO = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
-const numberStringFunction = function(perse,string) {
-  const parse = parseInt(string, 10);
-  return parse;
-};
-numberStringFunction(2023,'2022 год');
-// console.log(fun);
 
-// имяФункции('2023 год');            // 2023
-// имяФункции('ECMAScript 2022');     // 2022
-// имяФункции('1 кефир, 0.5 батона'); // 105
-// имяФункции('агент 007');           // 7
-// имяФункции('а я томат');           // NaN
+const randomPhotos = Math.floor(Math.random() * (25 - 1));
+const randomNumber = Math.floor(Math.random() * (5 - 1));
+const randomName = Math.floor(Math.random() * (4 - 1));
 
-let mass = [1,2,45,1,];
+function commentFotos () {
+  const randomComments = Math.floor(Math.random() * (9 - 1));
+  const randomNames = Math.floor(Math.random() * (4 - 1));
+  return {
+    id: 2,
+    avatar: `img/avatar-1${NUMBERS_PHOTO}.svg`,
+    message: COMMENT_LISTS[randomComments],
+    name:  NAMES[randomNames],
+  };
+}
+const functionComments = commentFotos();
+
+function fotosAvatars (){
+  return {
+    avatar: `img/avatar-1${NUMBERS_PHOTO[randomPhotos]}.svg`,
+  };
+}
+const fotoss = fotosAvatars ();
+
+function functionInfoPhotos () {
+  const idRandom = Math.floor(Math.random() * (25 - 1));
+  let date = {
+    id: idRandom,
+    URL: [fotoss],
+    description:  DESCRIPTIONS[randomNumber],
+    likes: Math.floor(Math.random() * (200 - 15)),
+    name:  NAMES[randomName],
+    comments: [functionComments],
+  };
+  return date;
+}
+const datePhotos = functionInfoPhotos();
+
+const functions = [functionInfoPhotos()];
+NUMBERS_PHOTO.forEach((item) => {
+  functions.push(functionInfoPhotos());
+});
+console.log(functions);
+
